e3e2fb6a2ed7d92e8f50973c99fd1778bfacf306
shubhcode12 <shubhcode.dev@gmail.com>
2023-03-26 21:25:38 +0530

    fix : refresh token route

--- routes/token.js
+++ routes/token.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {getAccessToken, printHelloCron} = require('../controller/token');
+const {getAccessToken, printHelloCron, refreshAccessToken} = require('../controller/token');
 
 
 router.route("/access-token").post(getAccessToken);
+router.route("/refresh-access-token").post(refreshAccessToken);
 router.route("/hello").get(printHelloCron);
 
 module.exports = router;

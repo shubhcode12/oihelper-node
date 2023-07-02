87bbc94cd1f1f5b2c4cf11f92afe93f751f31147
shubhcode12 <shubhcode.dev@gmail.com>
2023-07-02 19:55:41 +0530

    temp shutdown cron

--- index.js
+++ index.js
@@ -109,18 +109,18 @@ async function fetchAndSaveUnderlyingValue() {
 }
 
 // Schedule the job to run every 20 seconds
-  cron.schedule("*/10 * * * * *", () => {
-    const currentTime = new Date();
-    const marketStartTime = new Date();
-    marketStartTime.setHours(9, 0, 0); // Set market start time to 9 am
-    const marketEndTime = new Date();
-    marketEndTime.setHours(21, 30, 0); // Set market end time to 3:30 pm
+  // cron.schedule("*/10 * * * * *", () => {
+  //   const currentTime = new Date();
+  //   const marketStartTime = new Date();
+  //   marketStartTime.setHours(9, 0, 0); // Set market start time to 9 am
+  //   const marketEndTime = new Date();
+  //   marketEndTime.setHours(21, 30, 0); // Set market end time to 3:30 pm
 
-    if (currentTime >= marketStartTime && currentTime <= marketEndTime) {
-      console.log("hello")
-      //fetchAndSaveUnderlyingValue();
-    }
-  });
+  //   if (currentTime >= marketStartTime && currentTime <= marketEndTime) {
+  //     console.log("hello")
+  //     //fetchAndSaveUnderlyingValue();
+  //   }
+  // });
 
 
 app.listen(port, () => console.log(`Oihelper app listening on port ${port}!`));

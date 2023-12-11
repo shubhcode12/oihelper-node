a6ef859334872177e5362f058d3ba059629e7c4e
shubhcode12 <shubhcode.dev@gmail.com>
2023-12-11 16:37:00 +0530

    fix : schedule time

--- shubh.js
+++ shubh.js
@@ -221,7 +221,7 @@ const processOptionData = async (symbol, allowedDays) => {
   if (
     allowedDays.includes(dayOfWeek) &&
     currentTime >= 9 * 60 + 15 && // 9:15 am  9 * 60 + 15
-    currentTime <= 23 * 60 + 30 // 3:30 pm  15 * 60 + 30
+    currentTime <= 15 * 60 + 30 // 3:30 pm  15 * 60 + 30
   ) {
     try {
       const strikesParamsRef = db.ref(symbol).child("strikesParams");
